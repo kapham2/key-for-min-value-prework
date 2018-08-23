1c3f20f9eea52e297a124ca4ce33bc6ca814bcf0
Kim Pham <pham.kim.a@gmail.com>
2018-08-23 21:07:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
   end
   
   name_hash.each do |key, value|
-    if value < value
+    if smallest_value < value
       smallest_key = key
       smallest_value = value
     end
