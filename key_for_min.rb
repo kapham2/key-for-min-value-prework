86f89663fd7c0edd32a21d6644b7dfac8f2274b2
Kim Pham <pham.kim.a@gmail.com>
2018-08-23 20:49:12 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  
+  
+  name_hash.each do |key, value|
+    min_value = value
+    max_value = value
+    if
+  end
+  
+  smallest_key
 end
